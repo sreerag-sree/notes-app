81e317f2da61ddf82ef17b8466b0c388311c8a89
sreerag <sreeragsreekuttan911@gmail.com>
2024-12-28 17:45:11 +0530

    colors changed

--- frontend/src/components/Head.jsx
+++ frontend/src/components/Head.jsx
@@ -3,16 +3,14 @@ import { Link } from 'react-router-dom';
 export default function Head() {
     const linkStyle = {
         textDecoration: 'none',
-        color: '#f5400f'
+        color: '#730ff5'
     }
   return (
     <nav>
         <div className="logo">
             <Link to={'/'} style={linkStyle}><h2>Note App</h2></Link>
         </div>
-        <div className="action">
-            <button>Logout</button>
-        </div>
+        
     </nav>
   )
 }
